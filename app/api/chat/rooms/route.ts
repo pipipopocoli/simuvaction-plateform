f9bb948a3030ba2fec5c8d4f436e42ef7f995b9e
Olivier Cloutier <oliviercloutier@MacBook-Air-de-Olivier.local>
2026-02-23 02:49:15 +0700

    fix(prisma): Use named imports for prisma client in Chat API routes to fix Vercel build

--- app/api/chat/rooms/route.ts
+++ app/api/chat/rooms/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { getUserSession } from "@/lib/server-auth";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export async function GET(req: NextRequest) {
     const session = await getUserSession();
