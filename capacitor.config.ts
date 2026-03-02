e08499ae227554efd916210c96db7df46725a0b2
Olivier Cloutier <oliviercloutier@MacBook-Air-de-Olivier.local>
2026-03-02 23:08:38 +0700

    fix: change capacitor webDir to public to satisfy sync requirement

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
     appId: 'com.simuvaction.app',
     appName: 'SimuVaction 26',
-    webDir: 'out',
+    webDir: 'public',
     server: {
-        androidScheme: 'https'
+        androidScheme: 'https',
+        url: 'https://simuvaction-plateform-nfl6ts93q-pipipopocolis-projects.vercel.app',
+        cleartext: true
     }
 };
 
